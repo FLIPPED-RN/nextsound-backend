50fdb8ab419e29c07a4b3270839820bec82c9b02
FLIPPED_RN <rnazarov142@gmail.com>
2026-04-24 04:56:58 +0200

    Added Swagger to the /api project | 240426

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NextSound Backend')
+    .setDescription("NextSound Api Descripion")
+    .setVersion("0.0.1")
+    .addTag('NextSound')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
