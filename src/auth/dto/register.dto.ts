406bf8727a27a74c1aba9af87fa7260aa4cd7b4a
FLIPPED_RN <rnazarov142@gmail.com>
2026-04-24 04:20:36 +0200

    RegisterDto fix | 24.04.26

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -5,7 +5,6 @@ import {
   MinLength,
   Validate,
 } from 'class-validator';
-import { IsPasswordsMatchingConstraint } from '../../libs/common/decorators/is-passwords-matching-constraint.decorator';
 
 export class RegisterDto {
   @IsString({ message: 'Имя должно быть строкой' })
@@ -27,14 +26,4 @@ export class RegisterDto {
     message: 'Пароль должен содержать минимум 6 символов',
   })
   password!: string;
-
-  @IsString({ message: 'Пароль подтверждения должен быть строкой' })
-  @IsNotEmpty({ message: 'Поле подтверждения пароля не может быть пустым' })
-  @MinLength(6, {
-    message: 'Пароль подтверждения должен содержать не менее 6 символов',
-  })
-  @Validate(IsPasswordsMatchingConstraint, {
-    message: 'Пароли не совпадают',
-  })
-  passwordRepeat!: string;
 }
