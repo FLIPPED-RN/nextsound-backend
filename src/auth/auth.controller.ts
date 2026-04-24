4f9723da5b9a5aa18e201d41099b68e658184188
FLIPPED_RN <rnazarov142@gmail.com>
2026-04-24 04:42:02 +0200

    Updated the AuthController controller, added a decorator for Users, added protection for roles | 240426

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,37 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { response } from 'express';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+
+  @UseGuards(AuthGuard('local'))
+  @HttpCode(200)
+  @Post('login')
+  async login(@Request() req, @Res({ passthrough: true }) response) {
+    const { user, token } = await this.authService.login(req.user)
+    response.cookie('token', token, {
+      maxAge: 48 * 60 * 60 * 1000,
+      httoOnly: true,
+      signed: true
+    })
+
+    return { messgages: 'Успешный вход', result: { user } }
+  }
+
+  @Post('signup')
+  async signUp(@Body() body: CreateUserDto) {
+    const user = await this.authService.create(body)
+    return { messgages: 'Пользователь создан', result: user }
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('logout')
+  async logout(@Request() req, @Res({ passthrough: true }) response) {
+    response.clearCookie('token')
+    return {}
+  }
 }
