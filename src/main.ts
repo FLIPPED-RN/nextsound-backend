fced778ddbd307e32f9c80e4bc9aa2ac882a8d14
FLIPPED-RN <rnazarov142@gmail.com>
2026-05-20 13:54:51 +0200

    update 20.05.26

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('NextSound Backend')
-    .setDescription("NextSound Api Descripion")
-    .setVersion("0.0.1")
+    .setDescription('NextSound Api Description')
+    .setVersion('0.0.1')
     .addTag('NextSound')
-    .build()
+    .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory)
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
